52c3497c515889efea6e1dd1d5e46feaf4d82e94
Krisha Kapadia <krishakapadia2004@gmail.com>
2025-06-17 13:56:02 +0530

    bye

--- musicData.js
+++ musicData.js
@@ -1,8 +1,25 @@
 // filepath: d:\Impact Training\groovy\Backend\musicData.js
 const musicLinks = [
-  { id: 1, title: "Song One", url: "https://example.com/song1.mp3" },
-  { id: 2, title: "Song Two", url: "https://example.com/song2.mp3" },
-  { id: 3, title: "Song Three", url: "https://example.com/song3.mp3" }
+  {"id":1,"title":"Monkeys Spinning Monkeys","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Monkeys%20Spinning%20Monkeys.mp3","duration":"2:19"},
+  {"id":2,"title":"Sneaky Snitch","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Sneaky%20Snitch.mp3","duration":"2:31"},
+  {"id":3,"title":"Cipher","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Cipher.mp3","duration":"3:11"},
+  {"id":4,"title":"Investigations","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Investigations.mp3","duration":"2:52"},
+  {"id":5,"title":"Marty Gots a Plan","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Marty%20Gots%20a%20Plan.mp3","duration":"2:18"},
+  {"id":6,"title":"Impact Moderato","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Impact%20Moderato.mp3","duration":"1:45"},
+  {"id":7,"title":"Carefree","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Carefree.mp3","duration":"2:12"},
+  {"id":8,"title":"Local Forecast","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Local%20Forecast.mp3","duration":"2:50"},
+  {"id":9,"title":"Cipher Night","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Cipher%20Night.mp3","duration":"3:05"},
+  {"id":10,"title":"Oddity","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Oddity.mp3","duration":"1:28"},
+  {"id":11,"title":"Monkeys Intro","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Monkeys%20Intro.mp3","duration":"0:45"},
+  {"id":12,"title":"Sneaky Adventure","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Sneaky%20Adventure.mp3","duration":"2:42"},
+  {"id":13,"title":"Stingy Macabre","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Stingy%20Macabre.mp3","duration":"2:05"},
+  {"id":14,"title":"Mischievous Tryouts","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Mischievous%20Tryouts.mp3","duration":"2:10"},
+  {"id":15,"title":"Sneaky Talk","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Sneaky%20Talk.mp3","duration":"1:52"},
+  {"id":16,"title":"Jazzy Frenchy","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Jazzy%20Frenchy.mp3","duration":"2:30"},
+  {"id":17,"title":"Monkeys Tambourine","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Monkeys%20Tambourine.mp3","duration":"1:04"},
+  {"id":18,"title":"Sneaky Elevator","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Sneaky%20Elevator.mp3","duration":"2:15"},
+  {"id":19,"title":"Sneaky Business","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Sneaky%20Business.mp3","duration":"2:00"},
+  {"id":20,"title":"Happy Alley","artist":"Kevin MacLeod","url":"https://incompetech.com/music/royalty-free/mp3-royaltyfree/Happy%20Alley.mp3","duration":"2:28"}
 ];
 
 module.exports = musicLinks;
